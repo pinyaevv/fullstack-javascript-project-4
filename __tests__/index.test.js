0923a325a38ace6e64020147db2731b457647442
Vladimir Azyzy <Avallakhh@gmail.com>
2024-11-23 22:34:38 +0300

    Page Loader Step 2

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,15 @@
 import fs from 'fs/promises';
-import path from 'path';
+import path, { dirname, join } from 'path';
 import nock from 'nock';
 import os from 'os';
 import downloadPage from '../src/downloadPage.js';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
+const getDataFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 let tempDir;
 
@@ -50,4 +57,26 @@ test('download another page and save it', async () =>{
 
     const fileData = await fs.readFile(filePath, 'utf-8');
     expect(fileData).toBe(dataExpected);
+});
+
+test('download page and image', async () =>{
+  const url = 'https://ru.hexlet.io/courses';
+  const dataFile = getDataFile('page.html');
+  const dataExpected = getDataFile('expectedPage.html');
+  const pathFileImage = path.join(tempDir, 'ru-hexlet-io-courses_files', 'ru-hexlet-io-assets-professions-nodejs.png');
+
+  nock('https://ru.hexlet.io')
+    .get('/courses')
+    .reply(200, dataFile);
+  
+  const filePath = await downloadPage(url, tempDir);
+  
+  const fileExists = await fs
+    .access(pathFileImage)
+    .then(() => true)
+    .catch(() => false);
+  expect(fileExists).toBe(true);
+
+  const fileData = await fs.readFile(filePath, 'utf-8');
+  expect(fileData).toBe(dataExpected);
 });
