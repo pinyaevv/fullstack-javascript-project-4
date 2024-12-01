22d6a52057ae1659447a3ced7df707a645aa9967
Vladimir Azyzy <Avallakhh@gmail.com>
2024-12-01 23:08:25 +0300

    Page Loader Step 3.0. Add test "downloading other resources" + fixtures (css, js)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -45,7 +45,7 @@ test('download page and save it', async () => {
     expect(fileData).toBe(dataExpected);
 });
 
-test('download another page and save it', async () =>{
+test('download another page and save it', async () => {
     const url = 'https://ru.hexlet.io/another-courses';
     const dataExpected = '<html><head></head><body>Another content</body></html>';
 
@@ -64,7 +64,7 @@ test('download another page and save it', async () =>{
     expect(fileData).toBe(dataExpected);
 });
 
-test('download page and image', async () =>{
+test('download page and image', async () => {
   const url = 'https://ru.hexlet.io/courses';
   const dataFile = await getDataFile('page.html');
   const dataExpected = await getDataFile('expectedPage.html');
@@ -95,4 +95,44 @@ test('download page and image', async () =>{
 
   const updatedHtml = await fs.readFile(filePath, 'utf-8');
   expect(updatedHtml).toContain('ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-nodejs.png');
+});
+
+test('downloading other resources', async () => {
+  const url = 'https://ru.hexlet.io/courses';
+  const dataFile = await getDataFile('page.html');
+  const dataFileExpected = await getDataFile('expectedPage.html');
+  const cssFile = await getDataFile('ru-hexlet-io-courses_files/application.css');
+  const jsFile = await getDataFile('ru-hexlet-io-courses_files/rutime.js');
+
+  const pathFileCss = path.join(
+    tempDir,
+    'ru-hexlet-io-courses_files',
+    'ru-hexlet-io-assets-application.css');
+
+  const pathFileJs = path.join(
+    tempDir,
+    'ru-hexlet-io-courses_files',
+    'ru-hexlet-io-packs-js-runtime.js');
+
+  nock(url)
+    .get('/assets/application.css')
+    .reply(200, cssFile);
+
+  nock(url)
+    .get('packs/js/runtime.js')
+    .reply(200, jsFile);
+
+  const filePath = await downloadPage(url, tempDir);
+
+  const fileExistsCss = await fs
+    resource.access([pathFileCss, pathFileJs])
+    .then(() => true)
+    .catch(() => false);
+    expect(fileExistsCss).toBe(true);
+
+  const fileData = await fs.readFile(filePath, 'utf-8');
+  expect(normalizeHtml(fileData)).toBe(normalizeHtml(dataFile));
+
+  const updatedHtml = await fs.readFile(fileData, 'utf-8');
+  expect(normalizeHtml(updatedHtml)).toBe(normalizeHtml(dataFileExpected));
 });
